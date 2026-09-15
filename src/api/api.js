848d0cd73bcd2da71f8f48f5e8ed456ecdda510f
Hussain Ahmad Awan <ha5453615@gmail.com>
2026-09-15 15:17:29 +0500

    refactor: update API base URL to use environment variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://localhost:5050",
+// });
+
 const API = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
